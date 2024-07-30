9114afb127fd571472e878860b8a25f8a456d890
Maxim Babichev <maksim.babichev95@gmail.com>
2024-07-30 23:08:35 +0300

    add fatal log

--- cmd/root.go
+++ cmd/root.go
@@ -2,7 +2,7 @@ package cmd
 
 import (
 	"context"
-	"os"
+	"log"
 
 	"github.com/spf13/cobra"
 )
@@ -14,6 +14,6 @@ var rootCmd = &cobra.Command{
 
 func Execute(ctx context.Context) {
 	if err := rootCmd.ExecuteContext(ctx); err != nil {
-		os.Exit(1)
+		log.Fatalln(err)
 	}
 }
